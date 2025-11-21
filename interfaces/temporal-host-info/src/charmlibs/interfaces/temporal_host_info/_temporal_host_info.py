63ef97f0408f03365561a4184335d6334373c205
Wyatt Rees <wyatt.rees@canonical.com>
2025-11-21 10:41:58 -0700

    fixes

--- interfaces/temporal-host-info/src/charmlibs/interfaces/temporal_host_info/_temporal_host_info.py
+++ interfaces/temporal-host-info/src/charmlibs/interfaces/temporal_host_info/_temporal_host_info.py
@@ -16,10 +16,21 @@
 
 import logging
 
-from ops import CharmEvents, RelationChangedEvent, RelationJoinedEvent, framework
+from ops import (
+    ConfigChangedEvent,
+    Handle,
+    LeaderElectedEvent,
+    RelationChangedEvent,
+    RelationJoinedEvent,
+    framework,
+)
 from ops.charm import CharmBase
+from ops.framework import EventBase, EventSource, ObjectEvents
+from ops.model import ActiveStatus, Relation, WaitingStatus
 
 logger = logging.getLogger(__name__)
+
+RELATION_NAME = 'temporal-host-info'
 
 
 class TemporalHostInfoProvider(framework.Object):
@@ -38,13 +49,13 @@ class TemporalHostInfoProvider(framework.Object):
         self.charm = charm
         self.port = port
         charm.framework.observe(
-            charm.on.host_info_relation_joined, self._on_host_info_relation_changed
+            charm.on[RELATION_NAME].relation_joined, self._on_host_info_relation_changed
         )
         charm.framework.observe(
-            charm.on.host_info_relation_changed, self._on_host_info_relation_changed
+            charm.on[RELATION_NAME].relation_changed, self._on_host_info_relation_changed
         )
-        charm.framework.observe(charm.on.leader_elected, self._on_host_info_relation_changed)
-        charm.framework.observe(charm.on.config_changed, self._on_host_info_relation_changed)
+        charm.framework.observe(charm.on.leader_elected, self._on_config_changed)
+        charm.framework.observe(charm.on.config_changed, self._on_config_changed)
 
     def _on_host_info_relation_changed(self, event: RelationChangedEvent | RelationJoinedEvent):
         """Update relation data.
@@ -60,13 +71,24 @@ class TemporalHostInfoProvider(framework.Object):
             event.relation.data[self.charm.app]['host'] = host
             event.relation.data[self.charm.app]['port'] = str(self.port)
 
+    def _on_config_changed(self, event: ConfigChangedEvent | LeaderElectedEvent):
+        """Update relation data on config change."""
+        logger.info('Config changed, updating temporal-host-info relation data')
+        if self.charm.unit.is_leader() and 'frontend' in str(self.charm.config['services']):
+            host = str(self.charm.config['external-hostname'])
+            for relation in self.charm.model.relations.get('temporal-host-info', []):
+                if binding := self.charm.model.get_binding(relation):
+                    host = host or str(binding.network.bind_address)
+                relation.data[self.charm.app]['host'] = host
+                relation.data[self.charm.app]['port'] = str(self.port)
 
-class TemporalHostInfoRelationReadyEvent(framework.EventBase):
+
+class TemporalHostInfoRelationReadyEvent(EventBase):
     """Event emitted when temporal-host-info relation is ready."""
 
     def __init__(
         self,
-        handle: framework.Handle,
+        handle: Handle,
         host: str,
         port: int,
     ):
@@ -76,18 +98,21 @@ class TemporalHostInfoRelationReadyEvent(framework.EventBase):
 
     def snapshot(self) -> dict[str, str | int]:
         """Return a snapshot of the event."""
-        return {'host': self.host, 'port': self.port}
+        data = super().snapshot()
+        data.update({'host': self.host, 'port': self.port})
+        return data
 
     def restore(self, snapshot: dict[str, str | int]) -> None:
         """Restore the event from a snapshot."""
+        super().restore(snapshot)
         self.host = snapshot['host']
         self.port = snapshot['port']
 
 
-class TemporalHostInfoRequirerCharmEvents(CharmEvents):
+class TemporalHostInfoRequirerCharmEvents(ObjectEvents):
     """List of events that the requirer charm can leverage."""
 
-    host_info_available = framework.EventSource(TemporalHostInfoRelationReadyEvent)
+    temporal_host_info_available = EventSource(TemporalHostInfoRelationReadyEvent)
 
 
 class TemporalHostInfoRequirer(framework.Object):
@@ -99,8 +124,10 @@ class TemporalHostInfoRequirer(framework.Object):
 
         self.host_info = TemporalHostInfoRequirer(self)
         # update container with new host info
-        self.framework.observe(self.host_info.on.host_info_available, self._update)
+        self.framework.observe(self.host_info.on.temporal_host_info_available, self._update)
     """
+
+    on = TemporalHostInfoRequirerCharmEvents()  # type: ignore[reportAssignmentType]
 
     def __init__(self, charm: CharmBase):
         """Create a new instance of the TemporalHostInfoProvider class.
@@ -110,15 +137,35 @@ class TemporalHostInfoRequirer(framework.Object):
         """
         super().__init__(charm, 'host_info_requirer')
         self.charm = charm
-        self.on = TemporalHostInfoRequirerCharmEvents()  # type: ignore[reportAssignmentType]
-        self.host: str | None = None
-        self.port: int | None = None
         charm.framework.observe(
-            charm.on.host_info_relation_joined, self._on_host_info_relation_changed
+            charm.on[RELATION_NAME].relation_joined, self._on_host_info_relation_changed
         )
         charm.framework.observe(
-            charm.on.host_info_relation_changed, self._on_host_info_relation_changed
+            charm.on[RELATION_NAME].relation_changed, self._on_host_info_relation_changed
         )
+
+    @property
+    def relations(self) -> list[Relation]:
+        """Return the relations for this interface."""
+        return self.charm.model.relations.get(RELATION_NAME, [])
+
+    @property
+    def host(self) -> str | None:
+        """Return the host from the relation data."""
+        for relation in self.relations:
+            if relation and relation.app:
+                return relation.data[relation.app].get('host', None)
+        return None
+
+    @property
+    def port(self) -> int | None:
+        """Return the port from the relation data."""
+        for relation in self.relations:
+            if relation and relation.app:
+                port_str = relation.data[relation.app].get('port', None)
+                if port_str is not None:
+                    return int(port_str)
+        return None
 
     def _on_host_info_relation_changed(self, event: RelationChangedEvent):
         """Handle the relation joined/changed events.
@@ -126,6 +173,12 @@ class TemporalHostInfoRequirer(framework.Object):
         :param: event: The relation event that triggered this handler.
         :type event: RelationChangedEvent | RelationJoinedEvent
         """
-        self.host = event.relation.data[event.relation.app]['host']
-        self.port = int(event.relation.data[event.relation.app]['port'])
-        self.on.host_info_available.emit(host=self.host, port=self.port)
+        try:
+            host = event.relation.data[event.relation.app]['host']
+            port = int(event.relation.data[event.relation.app]['port'])
+        except KeyError:
+            self.charm.unit.status = WaitingStatus('Waiting for temporal-host-info provider')
+            event.defer()
+            return
+        self.charm.unit.status = ActiveStatus()
+        self.on.temporal_host_info_available.emit(host=host, port=port)
--- interfaces/temporal-worker-consumer/src/charmlibs/interfaces/temporal_worker_consumer/_temporal_worker_consumer.py
+++ interfaces/temporal-worker-consumer/src/charmlibs/interfaces/temporal_worker_consumer/_temporal_worker_consumer.py
@@ -17,10 +17,14 @@
 
 import logging
 
-from ops import CharmEvents, RelationChangedEvent, RelationJoinedEvent, framework
+from ops import ConfigChangedEvent, Handle, RelationChangedEvent, RelationJoinedEvent, framework
 from ops.charm import CharmBase
+from ops.framework import EventBase, EventSource, ObjectEvents
+from ops.model import ActiveStatus, Relation, WaitingStatus
 
 logger = logging.getLogger(__name__)
+
+RELATION_NAME = 'temporal-worker-consumer'
 
 
 class TemporalWorkerConsumerProvider(framework.Object):
@@ -35,12 +39,12 @@ class TemporalWorkerConsumerProvider(framework.Object):
         self.charm = charm
 
         charm.framework.observe(
-            charm.on.worker_consumer_relation_joined, self._on_worker_consumer_changed
+            charm.on[RELATION_NAME].relation_joined, self._on_worker_consumer_changed
         )
         charm.framework.observe(
-            charm.on.worker_consumer_relation_changed, self._on_worker_consumer_changed
+            charm.on[RELATION_NAME].relation_changed, self._on_worker_consumer_changed
         )
-        charm.framework.observe(charm.on.config_changed, self._on_worker_consumer_changed)
+        charm.framework.observe(charm.on.config_changed, self._on_config_changed)
 
     def _on_worker_consumer_changed(self, event: RelationChangedEvent | RelationJoinedEvent):
         """Update relation data.
@@ -50,18 +54,29 @@ class TemporalWorkerConsumerProvider(framework.Object):
         """
         logger.info('Handling temporal-worker-consumer relation event')
         if self.charm.unit.is_leader():
+            event.relation.data[self.charm.app]['namespace'] = str(self.charm.config['namespace'])
+            event.relation.data[self.charm.app]['queue'] = str(self.charm.config['queue'])
+
+    def _on_config_changed(self, event: ConfigChangedEvent):
+        """Handle config changes by updating relation data.
+
+        :param: event: The config changed event that triggered this handler.
+        :type event: ConfigChangedEvent
+        """
+        logger.info('Config changed, updating temporal-worker-consumer relation data')
+        if self.charm.unit.is_leader():
             # Config could have changed, so update all relations
             for relation in self.charm.model.relations.get('worker-consumer', ()):
                 relation.data[self.charm.app]['namespace'] = str(self.charm.config['namespace'])
                 relation.data[self.charm.app]['queue'] = str(self.charm.config['queue'])
 
 
-class TemporalWorkerConsumerRelationReadyEvent(framework.EventBase):
+class TemporalWorkerConsumerRelationReadyEvent(EventBase):
     """Event emitted when worker-consumer relation is ready."""
 
     def __init__(
         self,
-        handle: framework.Handle,
+        handle: Handle,
         namespace: str,
         queue: str,
     ):
@@ -79,10 +94,10 @@ class TemporalWorkerConsumerRelationReadyEvent(framework.EventBase):
         self.queue = snapshot['queue']
 
 
-class TemporalWorkerConsumerRequirerCharmEvents(CharmEvents):
+class TemporalWorkerConsumerRequirerCharmEvents(ObjectEvents):
     """List of events that the worker-consumer requirer charm can leverage."""
 
-    worker_consumer_available = framework.EventSource(TemporalWorkerConsumerRelationReadyEvent)
+    worker_consumer_available = EventSource(TemporalWorkerConsumerRelationReadyEvent)
 
 
 class TemporalWorkerConsumerRequirer(framework.Object):
@@ -97,6 +112,8 @@ class TemporalWorkerConsumerRequirer(framework.Object):
         self.framework.observe(self.worker_consumer.on.worker_consumer_available, self._update)
     """
 
+    on = TemporalWorkerConsumerRequirerCharmEvents()  # type: ignore[reportAssignmentType]
+
     def __init__(self, charm: CharmBase):
         """Create a new instance of the TemporalWorkerConsumerRequirer class.
 
@@ -104,15 +121,33 @@ class TemporalWorkerConsumerRequirer(framework.Object):
         """
         super().__init__(charm, 'worker-consumer-requirer')
         self.charm = charm
-        self.on = TemporalWorkerConsumerRequirerCharmEvents()  # type: ignore[reportAssignmentType]
-        self.namespace: str | None = None
-        self.queue: str | None = None
         charm.framework.observe(
-            charm.on.worker_consumer_relation_joined, self._on_worker_consumer_relation_changed
+            charm.on[RELATION_NAME].relation_joined, self._on_worker_consumer_relation_changed
         )
         charm.framework.observe(
-            charm.on.worker_consumer_relation_changed, self._on_worker_consumer_relation_changed
+            charm.on[RELATION_NAME].relation_changed, self._on_worker_consumer_relation_changed
         )
+
+    @property
+    def relations(self) -> list[Relation]:
+        """Return the relations for this interface."""
+        return self.charm.model.relations.get(RELATION_NAME, [])
+
+    @property
+    def namespace(self) -> str | None:
+        """Return the namespace from the relation data."""
+        for relation in self.relations:
+            if relation and relation.app:
+                return relation.data[relation.app].get('namespace', None)
+        return None
+
+    @property
+    def queue(self) -> str | None:
+        """Return the queue from the relation data."""
+        for relation in self.relations:
+            if relation and relation.app:
+                return relation.data[relation.app].get('queue', None)
+        return None
 
     def _on_worker_consumer_relation_changed(self, event: RelationChangedEvent):
         """Retrieve relation data and emit worker_consumer_available event.
@@ -120,6 +155,12 @@ class TemporalWorkerConsumerRequirer(framework.Object):
         :param: event: The relation event that triggered this handler.
         :type event: RelationChangedEvent | RelationJoinedEvent
         """
-        self.namespace = event.relation.data[event.relation.app]['namespace']
-        self.queue = event.relation.data[event.relation.app]['queue']
-        self.on.worker_consumer_available.emit(namespace=self.namespace, queue=self.queue)
+        try:
+            namespace = event.relation.data[event.relation.app]['namespace']
+            queue = event.relation.data[event.relation.app]['queue']
+        except KeyError:
+            self.charm.unit.status = WaitingStatus('Waiting for temporal-worker-consumer provider')
+            event.defer()
+            return
+        self.charm.unit.status = ActiveStatus()
+        self.on.worker_consumer_available.emit(namespace=namespace, queue=queue)
