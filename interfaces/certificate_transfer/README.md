69e4ff53c488929603d3ef75380d19a537b90764
kayra1 <kayra.gemalmaz@canonical.com>
2025-12-17 11:52:28 +0300

    add docs to init and remove empty page from readme

--- interfaces/certificate_transfer/README.md
+++ interfaces/certificate_transfer/README.md
@@ -9,5 +9,3 @@ from charmlibs.interfaces import certificate_transfer
 ```
 
 See the [reference documentation](https://documentation.ubuntu.com/charmlibs/reference/charmlibs/interfaces/certificate_transfer).
-
-Also see the [usage documentation](https://charmhub.io/certificate-transfer-interface) on Charmhub. This documentation was written when the library was hosted on Charmhub, so some parts might not be directly applicable.
--- interfaces/certificate_transfer/src/charmlibs/interfaces/certificate_transfer/__init__.py
+++ interfaces/certificate_transfer/src/charmlibs/interfaces/certificate_transfer/__init__.py
@@ -16,8 +16,110 @@
 
 This is a port of ``certificate_transfer_interface.certificate_transfer`` v1.15.
 
-Learn more about how to use the TLS Certificates interface library by reading the
-[usage documentation on Charmhub](https://charmhub.io/certificate-transfer-interface/).
+This library contains the Requires and Provides classes for handling the
+certificate-transfer interface. It supports both v0 and v1 of the interface.
+
+For requirers, they will set version 1 in their application databag as a hint to
+the provider. They will read the databag from the provider first as v1, and fallback
+to v0 if the format does not match.
+
+For providers, they will check the version in the requirer's application databag,
+and send v1 if that version is set to 1, otherwise it will default to 0 for backwards
+compatibility.
+
+=================
+Getting Started
+=================
+
+From a charm directory, fetch the library using `charmcraft`:
+
+::
+
+    charmcraft fetch-lib charms.certificate_transfer_interface.v1.certificate_transfer
+
+=================
+Provider charm
+=================
+
+The provider charm is the charm providing public certificates to another charm that requires them.
+
+Example:
+
+::
+
+    from ops.charm import CharmBase, RelationJoinedEvent
+    from ops.main import main
+
+    from lib.charms.certificate_transfer_interface.v1.certificate_transfer import (
+        CertificateTransferProvides,
+    )
+
+    class DummyCertificateTransferProviderCharm(CharmBase):
+        def __init__(self, *args):
+            super().__init__(*args)
+            self.certificate_transfer = CertificateTransferProvides(self, "certificates")
+            self.framework.observe(
+                self.on.certificates_relation_joined, self._on_certificates_relation_joined
+            )
+
+        def _on_certificates_relation_joined(self, event: RelationJoinedEvent):
+            certificate = "my certificate"
+            self.certificate_transfer.add_certificates(certificate)
+
+
+    if __name__ == "__main__":
+        main(DummyCertificateTransferProviderCharm)
+
+
+=================
+Requirer charm
+=================
+The requirer charm is the charm requiring certificates from another charm that provides them.
+
+Example:
+
+::
+
+    import logging
+
+    from ops.charm import CharmBase
+    from ops.main import main
+
+    from lib.charms.certificate_transfer_interface.v1.certificate_transfer import (
+        CertificatesAvailableEvent,
+        CertificatesRemovedEvent,
+        CertificateTransferRequires,
+    )
+
+
+    class DummyCertificateTransferRequirerCharm(CharmBase):
+        def __init__(self, *args):
+            super().__init__(*args)
+            self.certificate_transfer = CertificateTransferRequires(self, "certificates")
+            self.framework.observe(
+                self.certificate_transfer.on.certificate_set_updated, self._on_certificates_available
+            )
+            self.framework.observe(
+                self.certificate_transfer.on.certificates_removed, self._on_certificates_removed
+            )
+
+        def _on_certificates_available(self, event: CertificatesAvailableEvent):
+            logging.info(event.certificates)
+            logging.info(event.relation_id)
+
+        def _on_certificates_removed(self, event: CertificatesRemovedEvent):
+            logging.info(event.relation_id)
+
+
+    if __name__ == "__main__":
+        main(DummyCertificateTransferRequirerCharm)
+
+You can integrate both charms by running:
+
+::
+
+    juju integrate <certificate_transfer provider charm> <certificate_transfer requirer charm>
+
 """
 
 from ._certificate_transfer import (
