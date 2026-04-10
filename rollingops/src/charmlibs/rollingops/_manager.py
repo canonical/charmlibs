015468cee4b3d17a360940ca45d0e11c98b40b29
Patricia Reinoso <patricia.reinoso@canonical.com>
2026-04-10 09:05:25 +0200

    feat: advanced rolling ops using etcd (#364)
    ## Context
    
    All the code on this PR is new
    
    This implementation is based on [DA241
    Spec](https://docs.google.com/document/d/1ez4h6vOOyHy5mu6xDblcBt8PPAtMe7MUp75MtgG1sns/edit?tab=t.0)
    
    - The content of
    `charmlibs/advanced-rollingops/src/charmlibs/advanced_rollingops/_dp_interfaces_v1.py`
    belongs to another library that is currently being migrated to charmlibs
    so you can ignore it for now.
    
    ## Summary
    
    This PR is the first part of the implementation of advanced rolling ops
    at cluster level.
    
    This PR includes:
    - Management of the client certificate used to connect to etcd
    - The leader unit creates a self-signed certificate with live of 50
    years
      - Share the certificate with the other units using a peer relation
    - Implementation of the integration with etcd
      - Share the mtls certificate
      - Observe the `resource_created` event 
      - Observe the `endpoints_changed` event
    - Management of a env file needed to connecto etcd via `etcdctl`
     
    This PR does not implement the locking mechanism. In here we only test
    that we can connect to etcd from each unit.
    
    ## Current workflow:
    
    1. The unit make a request
    2. A new background process is spawn
    3. The background process dispatches a Juju hook
    4. The unit observes that hook
    5. The unit writes and read a value in etcd
    6. If the unit was able to connect to etcd, it executes the "restart"
    function.
    
    This is a very simplified workflow to be able to test that the units
    from different apps can reach etcd.
    
    ## To do
    - Implement the actual locking mechanism
    - Figure out how to properly install etcdctl

---
+++ rollingops/src/charmlibs/rollingops/_manager.py
@@ -0,0 +1,208 @@
+# Copyright 2026 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import logging
+from typing import Any
+
+from ops import Relation
+from ops.charm import (
+    CharmBase,
+    RelationBrokenEvent,
+    RelationCreatedEvent,
+    RelationDepartedEvent,
+)
+from ops.framework import EventBase, Object
+
+from charmlibs.rollingops import _etcdctl as etcdctl
+from charmlibs.rollingops._models import (
+    RollingOpsEtcdNotConfiguredError,
+    RollingOpsInvalidLockRequestError,
+    RollingOpsKeys,
+    RollingOpsNoEtcdRelationError,
+)
+from charmlibs.rollingops._relations import EtcdRequiresV1, SharedClientCertificateManager
+from charmlibs.rollingops._worker import EtcdRollingOpsAsyncWorker
+
+logger = logging.getLogger(__name__)
+
+
+class RollingOpsLockGrantedEvent(EventBase):
+    """Custom event emitted when the background worker grants the lock."""
+
+
+class EtcdRollingOpsManager(Object):
+    """Rolling ops manager for clusters."""
+
+    def __init__(
+        self,
+        charm: CharmBase,
+        peer_relation_name: str,
+        etcd_relation_name: str,
+        cluster_id: str,
+        callback_targets: dict[str, Any],
+    ):
+        """Register our custom events.
+
+        params:
+            charm: the charm we are attaching this to.
+            peer_relation_name: peer relation used for rolling ops.
+            etcd_relation_name: the relation to integrate with etcd.
+            cluster_id: unique identifier for the cluster
+            callback_targets: mapping from callback_id -> callable.
+        """
+        super().__init__(charm, 'rolling-ops-manager')
+        self._charm = charm
+        self.peer_relation_name = peer_relation_name
+        self.etcd_relation_name = etcd_relation_name
+        self.callback_targets = callback_targets
+        self.charm_dir = charm.charm_dir
+
+        owner = f'{self.model.uuid}-{self.model.unit.name}'.replace('/', '-')
+        self.worker = EtcdRollingOpsAsyncWorker(
+            charm, peer_relation_name=peer_relation_name, owner=owner
+        )
+        self.keys = RollingOpsKeys.for_owner(cluster_id, owner)
+
+        self.shared_certificates = SharedClientCertificateManager(
+            charm,
+            peer_relation_name=peer_relation_name,
+        )
+
+        self.etcd = EtcdRequiresV1(
+            charm,
+            relation_name=etcd_relation_name,
+            cluster_id=self.keys.cluster_prefix,
+            shared_certificates=self.shared_certificates,
+        )
+
+        charm.on.define_event('rollingops_lock_granted', RollingOpsLockGrantedEvent)
+
+        self.framework.observe(
+            charm.on[self.peer_relation_name].relation_departed, self._on_peer_relation_departed
+        )
+        self.framework.observe(
+            charm.on[self.etcd_relation_name].relation_broken, self._on_etcd_relation_broken
+        )
+        self.framework.observe(
+            charm.on[self.etcd_relation_name].relation_created, self._on_etcd_relation_created
+        )
+        self.framework.observe(charm.on.rollingops_lock_granted, self._on_rollingop_granted)
+
+    @property
+    def _peer_relation(self) -> Relation | None:
+        """Return the peer relation for this charm."""
+        return self.model.get_relation(self.peer_relation_name)
+
+    @property
+    def _etcd_relation(self) -> Relation | None:
+        """Return the etcd relation for this charm."""
+        return self.model.get_relation(self.etcd_relation_name)
+
+    def _on_etcd_relation_created(self, event: RelationCreatedEvent) -> None:
+        """Check whether the snap-provided etcdctl command is available."""
+        if not etcdctl.is_etcdctl_installed():
+            logger.error('%s is not installed', etcdctl.ETCDCTL_CMD)
+            # TODO: fallback to peer relation implementation.
+
+    def _on_rollingop_granted(self, event: RollingOpsLockGrantedEvent) -> None:
+        """Handle the event when a rolling operation lock is granted.
+
+        If etcd is not yet configured, the operation is skipped.
+        """
+        if not self._peer_relation or not self._etcd_relation:
+            # TODO: handle this case. Fallback to peer relation.
+            return
+        try:
+            etcdctl.ensure_initialized()
+        except RollingOpsEtcdNotConfiguredError:
+            # TODO: handle this case. Fallback to peer relation.
+            return
+        logger.info('Received a rolling-op lock granted event.')
+        self._on_run_with_lock()
+
+    def _on_peer_relation_departed(self, event: RelationDepartedEvent) -> None:
+        """Handle a unit departing from the peer relation.
+
+        If the current unit is the one departing, stop the etcd worker
+        process to ensure a clean shutdown.
+        """
+        unit = event.departing_unit
+        if unit == self.model.unit:
+            self.worker.stop()
+
+    def _on_etcd_relation_broken(self, event: RelationBrokenEvent) -> None:
+        """Handle the etcd relation being fully removed.
+
+        This method stops the etcd worker process since the required
+        relation is no longer available.
+        """
+        self.worker.stop()
+
+    def request_async_lock(
+        self,
+        callback_id: str,
+        kwargs: dict[str, Any] | None = None,
+        max_retry: int | None = None,
+    ) -> None:
+        """This is a dummy function.
+
+        Here we spawn a new process that will trigger a Juju hook.
+        This function will be completely remade in the next PR.
+
+        Args:
+            callback_id: Identifier of the registered callback to execute when
+                the lock is granted.
+            kwargs: Optional keyword arguments passed to the callback when
+                executed. Must be JSON-serializable.
+            max_retry: Maximum number of retries for the operation.
+                - None: retry indefinitely
+                - 0: do not retry on failure
+
+        Raises:
+            RollingOpsInvalidLockRequestError: If the callback_id is not registered or
+                invalid parameters were provided.
+            RollingOpsNoEtcdRelationError: if the etcd relation does not exist
+            RollingOpsEtcdNotConfiguredError: if etcd client has not been configured yet
+            PebbleConnectionError: if the remote container cannot be reached.
+            RollingOpsCharmLibMissingError: if the charm libs cannot be found.
+        """
+        if callback_id not in self.callback_targets:
+            raise RollingOpsInvalidLockRequestError(f'Unknown callback_id: {callback_id}')
+
+        if not self._etcd_relation:
+            raise RollingOpsNoEtcdRelationError
+
+        etcdctl.ensure_initialized()
+
+        # TODO: implement actual lock request
+
+        self.worker.start()
+
+    def _on_run_with_lock(self) -> None:
+        """This is a dummy function.
+
+        Here we try to reach etcd from each unit.
+        This function will be completely remade in the next PR.
+        """
+        # TODO: implement the actual execution under lock
+        etcdctl.run('put', self.keys.lock_key, self.keys.owner)
+
+        result = etcdctl.run('get', self.keys.lock_key, '--print-value-only')
+
+        if result is None:
+            logger.error('Unexpected response from etcd.')
+            return
+
+        callback = self.callback_targets.get('_restart', '')
+        callback(delay=1)
---
+++ rollingops/tests/integration/test_etcd_rolling_ops.py
@@ -0,0 +1,193 @@
+# Copyright 2026 Canonical Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Integration tests using real Juju and pre-packed charm(s)."""
+
+import json
+import logging
+from datetime import datetime
+from pathlib import Path
+
+import jubilant
+import pytest
+from tenacity import retry, stop_after_delay, wait_fixed
+
+TRACE_FILE = '/var/lib/charm-rolling-ops/transitions.log'
+logger = logging.getLogger(__name__)
+TIMEOUT = 15 * 60.0
+
+
+@retry(wait=wait_fixed(10), stop=stop_after_delay(60), reraise=True)
+def wait_for_etcdctl_env(juju: jubilant.Juju, unit: str) -> None:
+    task = juju.exec('test -f /var/lib/rollingops/etcd/etcdctl.json', unit=unit)
+    if task.status != 'completed' or task.return_code != 0:
+        raise RuntimeError('etcdctl config file not ready')
+
+
+def get_unit_events(juju: jubilant.Juju, unit: str) -> list[dict[str, str]]:
+    task = juju.exec(f'cat {TRACE_FILE}', unit=unit)
+
+    if not task.stdout.strip():
+        return []
+
+    return [json.loads(line) for line in task.stdout.strip().splitlines()]
+
+
+def parse_ts(event: dict[str, str]) -> datetime:
+    return datetime.fromisoformat(event['ts'])
+
+
+def test_deploy(juju: jubilant.Juju, app_name: str):
+    """The deployment takes place in the module scoped `juju` fixture."""
+    assert app_name in juju.status().apps
+
+
+@pytest.mark.machine_only
+def test_restart_action_one_unit(juju: jubilant.Juju, app_name: str):
+    """Verify that restart action runs through the expected workflow."""
+
+    juju.deploy(
+        'self-signed-certificates',
+        app='self-signed-certificates',
+        channel='1/stable',
+    )
+    juju.deploy(
+        'charmed-etcd',
+        app='etcd',
+        channel='3.6/stable',
+    )
+    juju.wait(jubilant.all_active, error=jubilant.any_error, timeout=TIMEOUT)
+
+    juju.integrate(
+        'etcd:client-certificates',
+        'self-signed-certificates:certificates',
+    )
+    juju.wait(jubilant.all_active, error=jubilant.any_error, timeout=TIMEOUT)
+
+    juju.integrate(f'{app_name}:etcd', 'etcd:etcd-client')
+    juju.wait(jubilant.all_active, error=jubilant.any_error, timeout=TIMEOUT)
+
+    wait_for_etcdctl_env(juju, f'{app_name}/0')
+
+    juju.run(f'{app_name}/0', 'restart', {'delay': 1}, wait=300)
+
+    juju.wait(
+        jubilant.all_active,
+        error=jubilant.any_error,
+        timeout=TIMEOUT,
+    )
+
+    events = get_unit_events(juju, f'{app_name}/0')
+    restart_events = [e['event'] for e in events]
+
+    expected = [
+        'action:restart',
+        '_restart:start',
+        '_restart:done',
+    ]
+
+    assert expected == restart_events
+
+
+@pytest.mark.machine_only
+def test_all_units_can_connect_to_etcd(juju: jubilant.Juju, app_name: str):
+    juju.add_unit(app_name, num_units=2)
+    juju.wait(
+        lambda status: jubilant.all_active(status, app_name),
+        error=jubilant.any_error,
+        timeout=TIMEOUT,
+    )
+
+    status = juju.status()
+    units = sorted(status.apps[app_name].units)
+
+    for unit in units:
+        juju.exec(f'rm -f {TRACE_FILE}', unit=unit)
+
+    for unit in units:
+        juju.run(unit, 'restart', {'delay': 2}, wait=300)
+
+    juju.wait(
+        lambda status: jubilant.all_active(status, app_name, 'etcd', 'self-signed-certificates'),
+        error=jubilant.any_error,
+        timeout=TIMEOUT,
+    )
+
+    expected = [
+        'action:restart',
+        '_restart:start',
+        '_restart:done',
+    ]
+
+    for unit in units:
+        events = get_unit_events(juju, unit)
+        restart_events = [e['event'] for e in events]
+        assert restart_events == expected
+
+
+@pytest.mark.machine_only
+def test_all_units_can_connect_to_etcd_multi_app(juju: jubilant.Juju, charm: Path, app_name: str):
+    second_app = f'{app_name}-secondary'
+
+    juju.deploy(charm, app=second_app, num_units=3)
+    juju.wait(
+        lambda status: jubilant.all_active(status, second_app),
+        error=jubilant.any_error,
+        timeout=TIMEOUT,
+    )
+    juju.integrate(f'{second_app}:etcd', 'etcd:etcd-client')
+
+    juju.wait(
+        lambda status: jubilant.all_active(
+            status, app_name, second_app, 'etcd', 'self-signed-certificates'
+        ),
+        error=jubilant.any_error,
+        timeout=TIMEOUT,
+    )
+
+    primary_units = sorted(juju.status().apps[app_name].units.keys())
+    secondary_units = sorted(juju.status().apps[second_app].units.keys())
+    all_units: list[str] = primary_units + secondary_units
+
+    for unit in all_units:
+        juju.exec(f'rm -f {TRACE_FILE}', unit=unit)
+
+    for unit in all_units:
+        wait_for_etcdctl_env(juju, unit)
+
+    for unit in all_units:
+        juju.run(unit, 'restart', {'delay': 2}, wait=300)
+
+    juju.wait(
+        lambda status: jubilant.all_active(
+            status,
+            app_name,
+            second_app,
+            'etcd',
+            'self-signed-certificates',
+        ),
+        error=jubilant.any_error,
+        timeout=TIMEOUT,
+    )
+
+    expected = [
+        'action:restart',
+        '_restart:start',
+        '_restart:done',
+    ]
+
+    for unit in all_units:
+        events = get_unit_events(juju, unit)
+        restart_events = [e['event'] for e in events]
+        assert restart_events == expected
