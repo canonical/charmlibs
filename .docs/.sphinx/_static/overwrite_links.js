0178f6b32833c83bca342af99e3de9adf55daf18
David Wilding <david.wilding@canonical.com>
2026-06-04 11:21:28 +0800

    add script to overwrite links

---
+++ .docs/.sphinx/_static/overwrite_links.js
@@ -0,0 +1,26 @@
+// Our docs are proxied through to canonical.com/juju/docs/charmlibs.
+// This script overwrites any links-to-self that Read the Docs generates.
+
+// Replace oldDomain with newDomain
+const oldDomain = 'canonical-juju-charmlibs.readthedocs-hosted.com';
+const newDomain = 'canonical.com/juju/docs/charmlibs';
+
+// Use a MutationObserver to wait for the RTD flyout element to appear in the DOM
+const observer = new MutationObserver(function(mutations, obs) {
+    const rtdFlyout = document.querySelector('readthedocs-flyout');
+    if (!rtdFlyout) return;
+
+    obs.disconnect();
+
+    rtdFlyout.addEventListener('click', function() {
+        const shadowRoot = rtdFlyout.shadowRoot;
+        if (!shadowRoot) return;
+
+        const anchors = shadowRoot.querySelectorAll('a');
+        anchors.forEach(anchor => {
+            anchor.href = anchor.href.replace(new RegExp(oldDomain, 'g'), newDomain);
+        });
+    });
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
