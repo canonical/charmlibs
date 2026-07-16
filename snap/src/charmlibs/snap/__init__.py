425e54991087a9b8b1b6b7e6e570f59c603a9102
James Garner <james.garner@canonical.com>
2026-07-16 12:52:45 +1200

    docs: correct function names

--- snap/src/charmlibs/snap/__init__.py
+++ snap/src/charmlibs/snap/__init__.py
@@ -23,7 +23,7 @@ Also manage:
 
 - Automatic refreshes with :func:`hold` and :func:`unhold`.
 - Services with :func:`start`, :func:`stop`, and :func:`restart`.
-- Config with :func:`config_get`, :func:`config_set`, and :func:`config_unset`.
+- Config with :func:`get`, :func:`set`, and :func:`unset`.
 - Connections between snaps with :func:`connect` and :func:`disconnect`.
 - Application aliases with :func:`alias` and :func:`unalias`.
 
